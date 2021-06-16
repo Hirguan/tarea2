a6077111b45592a4624add6401ef169377b36350
Hirguan <fabricio.perez.energy@gmail.com>
2021-06-16 03:06:45 +0100

    Updated

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int num1 = 5;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+            
+            //Console.WriteLine(suma);
+            //string valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + suma);
+            //int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+        }
+
+    }
+}
